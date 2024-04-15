094da36276c760064d0e7ef1455146c0ca90a624
Khalafawy <mohamedkhalaf11@gmail.com>
2024-04-15 11:37:29 -0700

    divisible/2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    div_2 = []
+    for i in my_list:
+        if (i % 2 == 0):
+            div_2.append(True)
+        else:
+            div_2.append(False)
+    return div_2
