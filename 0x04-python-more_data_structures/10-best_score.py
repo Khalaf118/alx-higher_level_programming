4630d62165409dd245aa306cee0f8dac17321758
Khalafawy <mohamedkhalaf11@gmail.com>
2024-04-18 13:54:29 -0700

    best_key

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    keys = list(a_dictionary)
+    best = keys[0]
+    for k in keys:
+        if a_dictionary[k] > a_dictionary[best]:
+            best = k
+    return best
