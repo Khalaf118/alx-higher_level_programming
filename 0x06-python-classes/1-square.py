9bbac4c937fd34d6799fa5833367803a0aa1f14a
Khalafawy <mohamedkhalaf11@gmail.com>
2024-04-26 14:46:23 -0700

    Square_size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 0-square.py
+# Mohamed Khalaf
+"""Define a class Square."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size):
+        """ Initialization the instance.
+
+        Args:
+            self: The instance arg.
+            size: The size of the square.
+
+        Returns:
+            No return values; None.
+
+        """
+
+        self.__size = size
