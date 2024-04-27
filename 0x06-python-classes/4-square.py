f70da358b4b7206efd58c67ebc1d470226e5bae6
Khalafawy <mohamedkhalaf11@gmail.com>
2024-04-27 08:00:42 -0700

    square_size_property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size=0):
+        """ Initialization the new square."""
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Update the size value."""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Returns the square area."""
+
+        return self.__size ** 2
